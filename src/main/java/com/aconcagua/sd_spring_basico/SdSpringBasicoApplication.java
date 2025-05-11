f5beb4b81f2e1ee1c43469b1bc8fd91d9cb1f1f7
Gabsplat <grperezdiez@gmail.com>
2025-05-11 12:11:47 -0300

    Repo inciado

---
+++ src/main/java/com/aconcagua/sd_spring_basico/SdSpringBasicoApplication.java
@@ -0,0 +1,14 @@
+package com.aconcagua.sd_spring_basico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdSpringBasicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdSpringBasicoApplication.class, args);
+		System.out.println("Todo bien");
+	}
+
+}
---
+++ src/test/java/com/aconcagua/sd_spring_basico/SdSpringBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aconcagua.sd_spring_basico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdSpringBasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
